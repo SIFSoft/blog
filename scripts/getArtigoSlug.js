b6db609f166765d9e1ed3e3bea05d2c78519b4d0
Your Nam <Jpsilvabarr@gmail.com>
2022-10-08 15:41:19 -0300

    0.0.2

--- scripts/getArtigoSlug.js
+++ scripts/getArtigoSlug.js
@@ -1,7 +1,7 @@
 
 async function getArtigoSlug(){
     let url = window.location.href;
-    let slug = url.split('#?')[1];
+    let slug = url.split('artigo/#')[1];
     console.log(slug)
     let api = `https://teste-api-sifsoft.herokuapp.com/artigo/${slug}/?format=json`
     const response = await fetch(api)
@@ -9,6 +9,7 @@ async function getArtigoSlug(){
     document.getElementById("titulo").textContent = data.titulo
     document.getElementById("resumo").textContent = data.resumo
     document.getElementById("texto").innerHTML = data.texto;
+    document.getElementById("nomeAutor").textContent = data.author
 }
 
 getArtigoSlug()
