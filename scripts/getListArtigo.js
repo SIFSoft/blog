716d93dc8a3090aa46972f766385b5d6442bdb39
Your Nam <Jpsilvabarr@gmail.com>
2022-10-09 10:15:47 -0300

    0.0.4

--- scripts/getListArtigo.js
+++ scripts/getListArtigo.js
@@ -1,4 +1,8 @@
 const url = 'https://teste-api-sifsoft.herokuapp.com/artigo/?format=json'
+
+var titulos = []
+var lista = document.getElementById("resultadosID")
+
 async function getAllArtigo(){
     const response = await fetch(url)
     const data = await response.json();
@@ -16,7 +20,26 @@ async function getAllArtigo(){
               </div>
             </div>
           </a>`
+          lista.innerHTML +=  `<li onclick="window.open('artigo/?${artigo.slug}', '_self')">${artigo.titulo}</li>`
+          titulos.push(artigo.titulo)
     });
 }
-
 getAllArtigo();
+
+function buscaArtigo(text){
+  let r = new RegExp(text, "g")
+  for(i in titulos){
+    if(titulos[i].match(r)){
+      lista.children[i].style.display = "block";
+    }else{
+      lista.children[i].style.display = "none";
+    }
+  }
+}
+
+function abrilista(){
+  lista.style.display = "block"
+}
+function fecharLista(){
+    lista.style.display = "none"
+}
