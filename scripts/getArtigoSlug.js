eff0b6e5f7326562777d3c81b82edeab0d7405d4
Your Nam <Jpsilvabarr@gmail.com>
2022-11-05 21:18:21 -0300

    0.1.2.5
    Matemática tex formula view.

--- scripts/getArtigoSlug.js
+++ scripts/getArtigoSlug.js
@@ -68,6 +68,13 @@ async function getArtigoSlug() {
     document.getElementById(
       "AtualizaçãoPost"
     ).textContent = `Atualizado em ${diaU} de ${meses[mesU]} de ${anoU}.`;
+    
+    if(document.querySelector('.math-tex') != null){
+      let script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML";
+      document.getElementsByTagName("head")[0].appendChild(script);
+    }
   }
 }
 
